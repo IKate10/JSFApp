fe462af1b40cda96d469dd47c54ff9de8fdfb593
1337BLENDER <savvin_evgenii@mail.ru>
2017-12-10 19:11:34 +0300

    картинка кликабельна. точки в правильных местах

--- src/main/webapp/resources/js/check.js
+++ src/main/webapp/resources/js/check.js
@@ -1,34 +1,24 @@
 
-function imgClickHandler(ClickEvent) {
-    var parrentPos = getPosition(ClickEvent.currentTarget);
-    var xPos = ClickEvent.clientX - parrentPos.x;
-    var yPos = ClickEvent.clientY - parrentPos.y;
-    var height = ClickEvent.currentTarget.clientHeight;
-    var width = ClickEvent.currentTarget.clientWidth;
-    r = document.getElementById('RVal').value;
-    var yVal = height / 2 - yPos;
-    var xVal = xPos - width / 2;
-    yVal = (10 * yVal) / height;
-    xVal = (10 * xVal) / width;
-    document.getElementById('mainForm:XVal').value = xVal;
-    document.getElementById('mainForm:YVal').value = yVal;
-    document.getElementById('mainForm:hiddenSubmit').click();
+function imgClickHandler(e) {
+    var x = e.offsetX==undefined?e.layerX:e.offsetX;
+    var y = e.offsetY==undefined?e.layerY:e.offsetY;
+    var r = $("#mainForm\\:RVal_input").val();
+    x=(x-220)/160*r;
+    y=(y-219)/-160*r;
+    document.getElementById("mainForm:XVal_input").value=x;
+    document.getElementById("mainForm:X").value=x;
+    document.getElementById("mainForm:YVal").value=y;
+    document.getElementById("mainForm:addButton").click();
 }
 
 
 function drawPoints() {
+    document.getElementById("mainForm:X").value=-10;//костыль
     var canvas=document.getElementById("canvas");
     var context=canvas.getContext("2d");
     var r = $("#mainForm\\:RVal_input").val();
-    //alert(r);
     context.clearRect(0,0,canvas.width, canvas.height);
-    var align = document.getElementById("canvas").getBoundingClientRect();
-    var midX=align.left+align.width/2;
-    var midY=align.top+align.height/2;
     var cou = 0;
-    var table = document.getElementById("mainForm:table");
-    var len=table.rows.length;
-    //alert(len);
     $("#mainForm\\:table").find("tr").each(function () {
         var x;
         var y;
@@ -49,11 +39,9 @@ function drawPoints() {
                         } else {
                             context.fillStyle = "#ff0000";
                         }
-                        //alert(isHitted);
-                        x = x * 93 / r + 127;
-                        y = y * (-93) / r + 127;
+                        x = x * 160 / r + 220;
+                        y = y * (-160) / r + 219;
                         context.fillRect(x, y, 2, 2);
-                        //alert(x + "|" + y);
                     }
                 }
                 c++;
@@ -63,6 +51,10 @@ function drawPoints() {
 
 }
 
+function cheat() {
+    if(document.getElementById("mainForm:X").value==-10)
+        document.getElementById("mainForm:X").value=document.getElementById("mainForm:XVal_input").value;
+}
 
 
 function addPoint() {
